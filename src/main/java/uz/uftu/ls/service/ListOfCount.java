260e7b0810cb497ee2df061b9670e5bedb73917b
KuronboevAsadbek <asadbek9805@gmail.com>
2024-02-25 21:22:04 +0500

    added some services such as List of Count etc.

---
+++ src/main/java/uz/uftu/ls/service/ListOfCount.java
@@ -0,0 +1,12 @@
+package uz.uftu.ls.service;
+
+import java.util.Map;
+
+public interface ListOfCount {
+
+     Map<String, Object> getListOfCount();
+
+     String getListOfCountByFaculty(Long facultyId);
+
+
+}
